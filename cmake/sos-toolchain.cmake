75e530d5e19e2fa865617e254e05b6e72772270b
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-04-20 10:32:22 -0600

    Add RPI cross compiling

--- cmake/sos-toolchain.cmake
+++ cmake/sos-toolchain.cmake
@@ -1,21 +1,31 @@
 
 message(STATUS "Checking Binary dir ${CMAKE_BINARY_DIR}")
 
-if( CMAKE_BINARY_DIR MATCHES ".*_link" )
-	set(SOS_TOOLCHAIN_BUILD_CONFIG link CACHE INTERNAL "sos build config is link")
-elseif( CMAKE_BINARY_DIR MATCHES ".*_arm" )
-	set(SOS_TOOLCHAIN_BUILD_CONFIG arm CACHE INTERNAL "sos build config is arm")
+if(CMAKE_BINARY_DIR MATCHES ".*_link")
+  set(SOS_TOOLCHAIN_BUILD_CONFIG link CACHE INTERNAL "sos build config is link")
+elseif(CMAKE_BINARY_DIR MATCHES ".*_arm")
+  set(SOS_TOOLCHAIN_BUILD_CONFIG arm CACHE INTERNAL "sos build config is arm")
+elseif(CMAKE_BINARY_DIR MATCHES ".*_rpi32")
+  set(SOS_TOOLCHAIN_BUILD_CONFIG rpi32 CACHE INTERNAL "sos build config is rpi32")
+elseif(CMAKE_BINARY_DIR MATCHES ".*_rpi64")
+  set(SOS_TOOLCHAIN_BUILD_CONFIG rpi64 CACHE INTERNAL "sos build config is rpi64")
 else()
-	message( FATAL_ERROR "No Configuration available build in *_link or *_arm directory")
+  message(FATAL_ERROR "No Configuration available build in *_link or *_arm directory")
 endif()
 
 string(COMPARE EQUAL ${SOS_TOOLCHAIN_BUILD_CONFIG} arm IS_ARM)
+string(COMPARE EQUAL ${SOS_TOOLCHAIN_BUILD_CONFIG} rpi32 IS_RPI32)
+string(COMPARE EQUAL ${SOS_TOOLCHAIN_BUILD_CONFIG} rpi64 IS_RPI64)
 message(STATUS "Toolchain SDK PATH: ${SOS_SDK_PATH} ARM:${IS_ARM}")
 
 if(IS_ARM)
-	include(${CMAKE_CURRENT_LIST_DIR}/toolchains/sos-gcc-toolchain.cmake)
+  include(${CMAKE_CURRENT_LIST_DIR}/toolchains/sos-gcc-toolchain.cmake)
+elseif(IS_RPI32)
+  include(${CMAKE_CURRENT_LIST_DIR}/toolchains/rpi32-linux-gcc-toolchain.cmake)
+elseif(IS_RPI64)
+  include(${CMAKE_CURRENT_LIST_DIR}/toolchains/rpi64-linux-gcc-toolchain.cmake)
 else()
-	include(${CMAKE_CURRENT_LIST_DIR}/toolchains/link-toolchain.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/toolchains/link-toolchain.cmake)
 endif()
 
 

43873e2277b2a48c1f62b4dee13572b2b041cd14
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-07-06 20:36:32 -0600

    Refactoring to CMSDK

--- cmake/sdk/cmsdk-variables.cmake
+++ cmake/sdk/cmsdk-variables.cmake
@@ -2,8 +2,8 @@
 option(CMSDK_VERBOSE "Create Verbose Makefiles | Application, OS Package, Library" OFF)
 
 # Used with sos-app-std, sos-lib-std
-option(CMSDK_ARCH_ARM_ALL "Build All configurations | use with sos-app-std.cmake or sos-lib-std.cmake to auto set CMSDK_ARCH" ON)
-option(CMSDK_ARCH_ARM_V7M "Build v7m configuration  | use with sos-app-std.cmake or sos-lib-std.cmake to auto set CMSDK_ARCH" OFF)
+option(CMSDK_ARCH_ARM_ALL "Build All configurations | use with cmsdk-app.cmake or cmsdk-lib.cmake to auto set CMSDK_ARCH" ON)
+option(CMSDK_ARCH_ARM_V7M "Build v7m configuration  | use with cmsdk-app.cmake or cmsdk-lib.cmake to auto set CMSDK_ARCH" OFF)
 option(CMSDK_ARCH_ARM_V7EM "Build v7em configuration  | use with sos-app-std.cmake or sos-lib-std.cmake to auto set CMSDK_ARCH" OFF)
 option(CMSDK_ARCH_ARM_V7EM_F4SH "Build v7em_f4sh configuration | use with sos-app-std.cmake or sos-lib-std.cmake to auto set CMSDK_ARCH" OFF)
 option(CMSDK_ARCH_ARM_V7EM_F5SH "Build v7em_f5sh configuration | use with sos-app-std.cmake or sos-lib-std.cmake to auto set CMSDK_ARCH" OFF)

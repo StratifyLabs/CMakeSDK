c83ac4c5bc4aee0b881277006c64a619988f213b
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-10-02 10:04:44 -0500

    Optimize cmsdk2 calls

--- cmake/sdk/cmsdk2-app.cmake
+++ cmake/sdk/cmsdk2-app.cmake
@@ -1,43 +1,45 @@
-function(cmsdk2_app_add_executable)
+function(cmsdk2_add_executable)
   set(OPTIONS "")
   set(PREFIX ARGS)
   set(ONE_VALUE_ARGS TARGET NAME OPTION CONFIG ARCH)
   set(MULTI_VALUE_ARGS "")
   cmake_parse_arguments(PARSE_ARGV 0 ${PREFIX} "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
-
   set(REQUIRED_ARGS TARGET NAME CONFIG ARCH)
   foreach(VALUE ${REQUIRED_ARGS})
     if(NOT ARGS_${VALUE})
       message(FATAL_ERROR "cmsdk2_app_target requires ${VALUE}")
     endif()
   endforeach()
-
   cmsdk2_internal_build_target_name(
     NAME ${ARGS_NAME}
     OPTION ${ARGS_OPTION}
     CONFIG ${ARGS_CONFIG}
     ARCH ${ARGS_ARCH}
     RESULT TARGET_NAME
-    BUILD_FOLDER TARGET_BUILD_FOLDER
-  )
-
+    BUILD_FOLDER TARGET_BUILD_FOLDER)
   add_executable(${TARGET_NAME})
-
   if(ARGS_OPTION)
     set_target_properties(${TARGET_NAME} PROPERTIES
-      CMSDK_PROPERTY_OPTION ${ARGS_OPTION}
-      )
+      CMSDK_PROPERTY_OPTION ${ARGS_OPTION})
   endif()
-
   set_target_properties(${TARGET_NAME} PROPERTIES
     CMSDK_PROPERTY_NAME ${ARGS_NAME}
     CMSDK_PROPERTY_CONFIG ${ARGS_CONFIG}
     CMSDK_PROPERTY_ARCH ${ARGS_ARCH}
-    CMSDK_PROPERTY_BUILD_FOLDER ${TARGET_BUILD_FOLDER}
-    )
+    CMSDK_PROPERTY_BUILD_FOLDER ${TARGET_BUILD_FOLDER})
 
+  if(OPTION)
+    target_compile_definitions(${TARGET_NAME}
+      PUBLIC
+      __${OPTION}
+      )
+  endif()
+  target_compile_definitions(${TARGET_NAME}
+    PUBLIC
+    ___${ARGS_CONFIG}
+    __${ARGS_ARCH}
+    CMSDK_BUILD_GIT_HASH=${CMSDK_GIT_HASH})
   set(${ARGS_TARGET} ${TARGET_NAME} PARENT_SCOPE)
-
 endfunction()
 
 function(cmsdk2_app_add_targets_for_architectures)
@@ -85,7 +87,6 @@ function(cmsdk2_app_add_targets_for_architectures)
       cmsdk2_internal_is_arch_enabled(
         ARCH ${ARCH}
         RESULT ARCH_ENABLED)
-
       if(${ARCH_ENABLED})
         cmsdk2_add_executable(
           TARGET TARGET_NAME
@@ -137,98 +138,59 @@ function(cmsdk2_app_update_target_for_architecture)
   set(ONE_VALUE_ARGS TARGET RAM_SIZE)
   set(MULTI_VALUE_ARGS "")
   cmake_parse_arguments(PARSE_ARGV 0 ${PREFIX} "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
-
   set(REQUIRED_ARGS TARGET)
   foreach(VALUE ${REQUIRED_ARGS})
     if(NOT ARGS_${VALUE})
       message(FATAL_ERROR "cmsdk2_update_target_for_architecture requires ${VALUE}")
     endif()
   endforeach()
-
   set(TARGET_NAME ${ARGS_TARGET})
   get_target_property(NAME ${TARGET_NAME} CMSDK_PROPERTY_NAME)
   get_target_property(OPTION ${TARGET_NAME} CMSDK_PROPERTY_OPTION)
   get_target_property(CONFIG ${TARGET_NAME} CMSDK_PROPERTY_CONFIG)
   get_target_property(ARCH ${TARGET_NAME} CMSDK_PROPERTY_ARCH)
   get_target_property(TARGET_BUILD_FOLDER ${TARGET_NAME} CMSDK_PROPERTY_BUILD_FOLDER)
-
   set(BINARY_OUTPUT_DIR ${CMAKE_CURRENT_SOURCE_DIR}/${TARGET_BUILD_FOLDER})
-
   set_target_properties(${TARGET_NAME}
     PROPERTIES RUNTIME_OUTPUT_DIRECTORY ${BINARY_OUTPUT_DIR})
-
   file(MAKE_DIRECTORY ${BINARY_OUTPUT_DIR})
-
   if(ARCH STREQUAL "link")
     if(CMSDK_IS_WINDOWS OR CMSDK_IS_LINUX)
       target_link_options(${TARGET_NAME} PRIVATE -static-libstdc++ -static-libgcc)
     endif()
-
-    if(OPTION)
-      target_compile_definitions(${TARGET_NAME}
-        PUBLIC
-        __${OPTION}
-        )
-    endif()
-
-    target_compile_definitions(${TARGET_NAME}
-      PUBLIC
-      ___${CONFIG}
-      __${ARCH}
-      CMSDK_BUILD_GIT_HASH=${CMSDK_GIT_HASH}
-      )
-
     target_include_directories(${TARGET_NAME}
       PRIVATE
-      ${CMSDK_LOCAL_PATH}/include/StratifyOS
-      )
-
+      ${CMSDK_LOCAL_PATH}/include/StratifyOS)
   else()
-
     # load ARCH specific variables into current scope
     cmsdk2_internal_get_arm_arch(
       ARCHITECTURE ${ARCH}
       FLOAT_OPTIONS BUILD_FLOAT_OPTIONS
       FLOAT_DIRECTORY BUILD_FLOAT_DIRECTORY
-      INSTALL_DIRECTORY BUILD_INSTALL_DIRECTORY
-    )
-
+      INSTALL_DIRECTORY BUILD_INSTALL_DIRECTORY)
     target_compile_definitions(${TARGET_NAME}
       PUBLIC
-      __StratifyOS__
-      __${CONFIG}
-      __${OPTION}
-      __${ARGS_ARCH}
-      CMSDK_BUILD_GIT_HASH=${CMSDK_GIT_HASH}
-      )
-
+      __StratifyOS__)
     target_include_directories(${TARGET_NAME}
       PUBLIC
       ${CMSDK_BUILD_SYSTEM_INCLUDES}
       PRIVATE
-      ${CMSDK_LOCAL_PATH}/arm-none-eabi/include/StratifyOS
-      )
-
+      ${CMSDK_LOCAL_PATH}/arm-none-eabi/include/StratifyOS)
     target_compile_options(${TARGET_NAME}
       PUBLIC
       -mthumb -mlong-calls -ffunction-sections -fdata-sections
-      ${BUILD_FLOAT_OPTIONS}
-      )
-
+      ${BUILD_FLOAT_OPTIONS})
     get_target_property(EXIST_LINK_FLAGS ${TARGET_NAME} LINK_FLAGS)
     if(EXIST_LINK_FLAGS STREQUAL "EXIST_LINK_FLAGS-NOTFOUND")
       unset(EXIST_LINK_FLAGS)
     endif()
-
     cmsdk2_internal_build_target_name(
       NAME ${NAME}
       OPTION ${OPTION}
       CONFIG ${CONFIG}
       ARCH ${ARCH}
       RESULT INSTALL_NAME
-      BUILD_FOLDER BUILD_FOLDER
-    )
-
+      BUILD_FOLDER BUILD_FOLDER)
     set(UPDATED_LINK_FLAGS
       -L${CMSDK_LOCAL_PATH}/arm-none-eabi/lib/${BUILD_INSTALL_DIRECTORY}/${BUILD_FLOAT_DIRECTORY}
       -L${CMSDK_LOCAL_PATH}/lib/gcc/arm-none-eabi/${CMAKE_CXX_COMPILER_VERSION}/${BUILD_INSTALL_DIRECTORY}/${BUILD_FLOAT_DIRECTORY}
@@ -237,22 +199,15 @@ function(cmsdk2_app_update_target_for_architecture)
       -nostartfiles
       -nostdlib
       -u crt
-      ${EXIST_LINK_FLAGS}
-      )
-
+      ${EXIST_LINK_FLAGS})
     list(JOIN UPDATED_LINK_FLAGS " " LINK_FLAGS)
-
     set_target_properties(${TARGET_NAME}
       PROPERTIES
       LINK_FLAGS
-      "${LINK_FLAGS}"
-      )
-
+      "${LINK_FLAGS}")
     add_custom_target(bin_${TARGET_NAME} DEPENDS ${TARGET_NAME} COMMAND ${CMAKE_OBJCOPY} -j .text -j .data -O binary ${BINARY_OUTPUT_DIR}/${TARGET_NAME} ${BINARY_OUTPUT_DIR}/${CMSDK_SDK_TMP_NO_CONFIG})
     add_custom_target(asm_${TARGET_NAME} DEPENDS bin_${TARGET_NAME} COMMAND ${CMAKE_OBJDUMP} -S -j .text -j .priv_code -j .data -j .bss -d ${BINARY_OUTPUT_DIR}/${TARGET_NAME} > ${BINARY_OUTPUT_DIR}/${INSTALL_NAME}.lst)
     add_custom_target(size_${TARGET_NAME} DEPENDS asm_${TARGET_NAME} COMMAND ${CMAKE_SIZE} ${BINARY_OUTPUT_DIR}/${TARGET_NAME})
     add_custom_target(all_${TARGET_NAME} ALL DEPENDS size_${TARGET_NAME})
-
   endif()
-
 endfunction()

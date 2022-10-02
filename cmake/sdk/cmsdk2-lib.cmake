d29614221c00da2436ffd6edb345d437724b554e
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-10-02 09:49:56 -0500

    Optimize cmsdk2-lib

--- cmake/sdk/cmsdk2-lib.cmake
+++ cmake/sdk/cmsdk2-lib.cmake
@@ -4,14 +4,12 @@ function(cmsdk2_add_library)
 	set(ONE_VALUE_ARGS TARGET NAME OPTION CONFIG ARCH)
 	set(MULTI_VALUE_ARGS "")
 	cmake_parse_arguments(PARSE_ARGV 0 ${PREFIX} "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
-
 	set(REQUIRED_ARGS TARGET NAME CONFIG ARCH)
 	foreach(VALUE ${REQUIRED_ARGS})
 		if(NOT ARGS_${VALUE})
 			message(FATAL_ERROR "cmsdk2_add_library requires ${VALUE}")
 		endif()
 	endforeach()
-
 	cmsdk2_internal_build_target_name(
 		NAME ${ARGS_NAME}
 		OPTION ${ARGS_OPTION}
@@ -20,31 +18,72 @@ function(cmsdk2_add_library)
 		RESULT TARGET_NAME
 		BUILD_FOLDER TARGET_BUILD_FOLDER
 	)
-
 	add_library(${TARGET_NAME})
-
 	if(ARGS_OPTION)
 		set_target_properties(${TARGET_NAME} PROPERTIES
 			CMSDK_PROPERTY_OPTION ${ARGS_OPTION}
 			)
 	endif()
-
 	set_target_properties(${TARGET_NAME} PROPERTIES
 		CMSDK_PROPERTY_NAME ${ARGS_NAME}
 		CMSDK_PROPERTY_CONFIG ${ARGS_CONFIG}
 		CMSDK_PROPERTY_ARCH ${ARGS_ARCH}
 		CMSDK_PROPERTY_BUILD_FOLDER ${TARGET_BUILD_FOLDER}
 		)
+	target_compile_definitions(${TARGET_NAME}
+		PRIVATE
+		__${ARGS_ARCH}
+		___${ARGS_CONFIG}
+		CMSDK_BUILD_GIT_HASH=${CMSDK_GIT_HASH}
+		)
+	if(OPTION)
+		target_compile_definitions(${TARGET_NAME}
+			PRIVATE
+			__${OPTION}
+			)
+	endif()
+	if(${CMSDK_IS_ARM})
+		cmsdk2_internal_get_arm_arch(
+			ARCHITECTURE ${ARGS_ARCH}
+			FLOAT_OPTIONS ARCH_BUILD_FLOAT_OPTIONS
+		)
+		target_compile_definitions(${TARGET_NAME}
+			PUBLIC
+			__StratifyOS__
+			)
+		target_compile_options(${TARGET_NAME}
+			PRIVATE
+			-mthumb -ffunction-sections -fdata-sections
+			${ARCH_BUILD_FLOAT_OPTIONS}
+			)
+		set_target_properties(${TARGET_NAME}
+			PROPERTIES NO_SYSTEM_FROM_IMPORTED TRUE
+			)
+		target_include_directories(${TARGET_NAME}
+			PRIVATE
+			${CMSDK_LOCAL_PATH}/arm-none-eabi/include/StratifyOS
+			)
+	endif()
+
+	get_target_property(TARGET_BINARY_DIR ${TARGET_NAME} BINARY_DIR)
+
+	install(
+		TARGETS ${TARGET_NAME}
+		EXPORT ${TARGET_NAME}
+		DESTINATION lib
+		OPTIONAL)
+	install(
+		EXPORT ${TARGET_NAME}
+		DESTINATION ${CMSDK_LOCAL_PATH}/cmake/targets)
 
 	set(${ARGS_TARGET} ${TARGET_NAME} PARENT_SCOPE)
-
 endfunction()
 
-function(cmsdk2_library_add_targets_for_architectures)
+function(cmsdk2_library_add_dependencies)
 	set(OPTIONS "")
 	set(PREFIX ARGS)
 	set(ONE_VALUE_ARGS TARGET)
-	set(MULTI_VALUE_ARGS DEPENDENCIES TARGETS)
+	set(MULTI_VALUE_ARGS DEPENDENCIES TARGETS ARCHITECTURES)
 	cmake_parse_arguments(PARSE_ARGV 0 ${PREFIX} "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
 
 	set(REQUIRED_ARGS TARGET)
@@ -61,10 +100,6 @@ function(cmsdk2_library_add_targets_for_architectures)
 	get_target_property(TARGET_BUILD_FOLDER ${ARGS_TARGET} CMSDK_PROPERTY_BUILD_FOLDER)
 
 	if(${CMSDK_IS_LINK})
-
-		cmsdk2_update_library_for_architecture(
-			TARGET ${ARGS_TARGET}
-		)
 
 		if(ARGS_TARGETS)
 			set(${ARGS_TARGETS} ${ARGS_TARGET} PARENT_SCOPE)
@@ -84,7 +119,7 @@ function(cmsdk2_library_add_targets_for_architectures)
 			set(${ARGS_TARGETS} ${ARGS_TARGET} PARENT_SCOPE)
 		endif()
 
-		foreach (ARCH ${CMSDK_ARCH_LIST})
+		foreach (ARCH ${ARGS_ARCHITECTURES})
 			cmsdk_internal_is_arch_enabled(${ARCH})
 			if(ARCH_ENABLED)
 				set(TARGET_NAME ${BASE_NAME})
@@ -108,11 +143,6 @@ function(cmsdk2_library_add_targets_for_architectures)
 					DESTINATION ${TARGET_NAME}
 				)
 
-				# this applies architecture specific options
-				cmsdk2_update_library_for_architecture(
-					TARGET ${TARGET_NAME}
-				)
-
 				foreach(DEPENDENCY ${DEPENDENCIES})
 					target_link_libraries(${ARGS_TARGET}
 						PUBLIC
@@ -122,10 +152,6 @@ function(cmsdk2_library_add_targets_for_architectures)
 				endforeach()
 			endif()
 		endforeach(ARCH)
-
-		cmsdk2_update_library_for_architecture(
-			TARGET ${ARGS_TARGET}
-		)
 
 		foreach(DEPENDENCY ${DEPENDENCIES})
 			target_link_libraries(${ARGS_TARGET}
@@ -138,79 +164,3 @@ function(cmsdk2_library_add_targets_for_architectures)
 
 endfunction()
 
-
-function(cmsdk2_update_library_for_architecture)
-	set(OPTIONS "")
-	set(PREFIX ARGS)
-	set(ONE_VALUE_ARGS TARGET)
-	set(MULTI_VALUE_ARGS "")
-	cmake_parse_arguments(PARSE_ARGV 0 ${PREFIX} "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
-
-	set(REQUIRED_ARGS TARGET)
-	foreach(VALUE ${REQUIRED_ARGS})
-		if(NOT ARGS_${VALUE})
-			message(FATAL_ERROR "cmsdk2_update_library_for_architecture requires ${VALUE}")
-		endif()
-	endforeach()
-
-	set(TARGET_NAME ${ARGS_TARGET})
-	get_target_property(NAME ${TARGET_NAME} CMSDK_PROPERTY_NAME)
-	get_target_property(OPTION ${TARGET_NAME} CMSDK_PROPERTY_OPTION)
-	get_target_property(CONFIG ${TARGET_NAME} CMSDK_PROPERTY_CONFIG)
-	get_target_property(ARCH ${TARGET_NAME} CMSDK_PROPERTY_ARCH)
-	get_target_property(TARGET_BUILD_FOLDER ${TARGET_NAME} CMSDK_PROPERTY_BUILD_FOLDER)
-
-	target_compile_definitions(${TARGET_NAME}
-		PRIVATE
-		__${ARCH}
-		___${CONFIG}
-		CMSDK_BUILD_GIT_HASH=${CMSDK_GIT_HASH}
-		)
-
-	if(OPTION)
-		target_compile_definitions(${TARGET_NAME}
-			PRIVATE
-			__${OPTION}
-			)
-	endif()
-
-
-	if(CMSDK_IS_ARM)
-		cmsdk2_internal_get_arm_arch(
-			ARCHITECTURE ${ARCH}
-			FLOAT_OPTIONS ARCH_BUILD_FLOAT_OPTIONS
-		)
-
-		target_compile_definitions(${TARGET_NAME}
-			PUBLIC
-			__StratifyOS__
-			)
-
-		target_compile_options(${TARGET_NAME}
-			PRIVATE
-			-mthumb -ffunction-sections -fdata-sections
-			${ARCH_BUILD_FLOAT_OPTIONS}
-			)
-
-		set_target_properties(${TARGET_NAME}
-			PROPERTIES NO_SYSTEM_FROM_IMPORTED TRUE
-			)
-
-		target_include_directories(${TARGET_NAME}
-			PRIVATE
-			${CMSDK_LOCAL_PATH}/arm-none-eabi/include/StratifyOS
-			)
-	endif()
-
-	get_target_property(TARGET_BINARY_DIR ${TARGET_NAME} BINARY_DIR)
-
-	install(
-		TARGETS ${TARGET_NAME}
-		EXPORT ${TARGET_NAME}
-		DESTINATION lib
-		OPTIONAL)
-	install(
-		EXPORT ${TARGET_NAME}
-		DESTINATION ${CMSDK_LOCAL_PATH}/cmake/targets)
-
-endfunction()
--- cmake/sdk/cmsdk2.cmake
+++ cmake/sdk/cmsdk2.cmake
@@ -1,27 +1,31 @@
 function(cmsdk2_add_sources)
   set(OPTIONS "")
   set(PREFIX ARGS)
-  set(ONE_VALUE_ARGS DIRECTORY LIST BINARY_DIRECTORY)
+  set(ONE_VALUE_ARGS TARGET DIRECTORY BINARY_DIRECTORY VISIBILITY)
   set(MULTI_VALUE_ARGS DEPENDENCIES)
   cmake_parse_arguments(PARSE_ARGV 0 ${PREFIX} "${OPTIONS}" "${ONE_VALUE_ARGS}" "${MULTI_VALUE_ARGS}")
-
-  set(REQUIRED_ARGS DIRECTORY LIST)
+  set(REQUIRED_ARGS TARGET DIRECTORY)
   foreach(VALUE ${REQUIRED_ARGS})
     if(NOT ARGS_${VALUE})
       message(FATAL_ERROR "cmsdk2_add_sources requires ${VALUE}")
     endif()
   endforeach()
-
+  if(ARGS_VISIBILITY)
+    set(VISIBILITY ${ARGS_VISIBILITY})
+  else()
+    set(VISIBILITY PRIVATE)
+  endif()
   if(ARGS_BINARY_DIRECTORY)
     add_subdirectory(${ARGS_DIRECTORY} ${ARGS_BINARY_DIRECTORY})
   else()
     add_subdirectory(${ARGS_DIRECTORY})
   endif()
-
   #SOURCES comes from the CMakeLists.txt file in ${ARGS_DIRECTORY}
   list(TRANSFORM SOURCES PREPEND ${ARGS_DIRECTORY}/)
   list(APPEND ${ARGS_LIST} ${SOURCES})
-  set(${ARGS_LIST} ${${ARGS_LIST}} PARENT_SCOPE)
+  target_sources(${ARGS_TARGET}
+    ${VISIBILITY}
+    ${SOURCES})
 endfunction()
 
 function(cmsdk2_copy_target)
@@ -36,7 +40,6 @@ function(cmsdk2_copy_target)
   if(NOT ARGS_SOURCE)
     message(FATAL_ERROR "cmsdk2_copy_target requires SOURCE")
   endif()
-
   foreach(DEST ${ARGS_DESTINATION})
     cmsdk_copy_target(${ARGS_SOURCE} ${DEST})
   endforeach()
@@ -69,25 +72,17 @@ function(cmsdk2_add_test)
     set(EXEC_NAME ${NAME})
     set(DIR_NAME build)
   endif()
-
   set(EXEC_NAME ${EXEC_NAME}_${CONFIG})
   set(TARGET_NAME ${EXEC_NAME}_${ARCH})
-
   get_target_property(TARGET_BINARY_DIR ${ARGS_TARGET} RUNTIME_OUTPUT_DIRECTORY)
-
   message(STATUS "add test -> ${TARGET_NAME}")
-
   add_test(NAME ${NAME}_${CONFIG}
-    COMMAND "${TARGET_BINARY_DIR}/${TARGET_NAME}" --api
-    )
-
+    COMMAND "${TARGET_BINARY_DIR}/${TARGET_NAME}" --api)
   set_tests_properties(
     ${NAME}_${CONFIG}
     PROPERTIES
-    PASS_REGULAR_EXPRESSION "___finalResultPass___"
-  )
+    PASS_REGULAR_EXPRESSION "___finalResultPass___")
 endfunction()
-
 
 include(${CMAKE_CURRENT_LIST_DIR}/cmsdk2-app.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/cmsdk2-lib.cmake)

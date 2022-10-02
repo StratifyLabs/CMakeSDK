8b5b3279456966b60562d520e03f1143b522ccce
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2022-10-02 10:29:10 -0500

    Minor cleanup

--- cmake/cmsdk.cmake
+++ cmake/cmsdk.cmake
@@ -24,13 +24,11 @@ function(cmsdk_git_clone_or_pull_branch BASE_DIRECTORY NAME REPOSITORY BRANCH)
   endif()
   add_custom_target(cmsdk_pull_${NAME}
     COMMAND git pull
-    WORKING_DIRECTORY ${BASE_DIRECTORY}/${NAME}
-    )
+    WORKING_DIRECTORY ${BASE_DIRECTORY}/${NAME})
   add_custom_target(cmsdk_checkout_${NAME}
     COMMAND git checkout ${BRANCH}
     WORKING_DIRECTORY ${BASE_DIRECTORY}/${NAME}
-    DEPENDS cmsdk_pull_${NAME}
-    )
+    DEPENDS cmsdk_pull_${NAME})
   if(CMSDK_PULL_TARGET)
     add_dependencies(${CMSDK_PULL_TARGET} cmsdk_checkout_${NAME})
   endif()
@@ -110,19 +108,15 @@ function(cmsdk_copy_target SOURCE_TARGET DEST_TARGET)
   foreach(PROPERTY ${CMSDK_SHARED_PROPERTIES})
     string(REPLACE "<CONFIG>" "${CMAKE_BUILD_TYPE}" prop ${PROPERTY})
     get_property(PROPERTY_VALUE TARGET ${SOURCE_TARGET} PROPERTY ${PROPERTY} SET)
-
     if(PROPERTY_VALUE)
       get_target_property(PROPERTY_VALUE ${SOURCE_TARGET} ${PROPERTY})
       set_property(TARGET ${DEST_TARGET} PROPERTY ${PROPERTY} ${PROPERTY_VALUE})
     endif()
-
   endforeach(PROPERTY)
 endfunction()
 
 function(cmsdk_add_test NAME OPTION CONFIG)
-
   string(COMPARE EQUAL ${OPTION} "" OPTION_IS_EMPTY)
-
   if(OPTION_IS_EMPTY)
     set(EXEC_NAME ${NAME})
     set(DIR_NAME build)
@@ -130,32 +124,22 @@ function(cmsdk_add_test NAME OPTION CONFIG)
     set(EXEC_NAME ${NAME}_${OPTION})
     set(DIR_NAME build_${OPTION})
   endif()
-
   set(EXEC_NAME ${EXEC_NAME}_${CONFIG})
   set(TARGET_NAME ${EXEC_NAME}_link.elf)
-
   get_target_property(TARGET_BINARY_DIR ${TARGET_NAME} RUNTIME_OUTPUT_DIRECTORY)
-
   message(STATUS "CMSDK - Add test ${TARGET_BINARY_DIR}/${TARGET_NAME}")
-
   add_test(NAME ${NAME}_${CONFIG}
-    COMMAND "${TARGET_BINARY_DIR}/${TARGET_NAME}" --api
-    )
-
+    COMMAND "${TARGET_BINARY_DIR}/${TARGET_NAME}" --api)
   set_tests_properties(
     ${NAME}_${CONFIG}
     PROPERTIES
-    PASS_REGULAR_EXPRESSION "___finalResultPass___"
-  )
-
+    PASS_REGULAR_EXPRESSION "___finalResultPass___")
 endfunction()
-
 
 function(cmsdk_exclude_arch_from_all TARGET ARCH_LIST)
   foreach(ARCH ${ARCH_LIST})
     set_target_properties(${TARGET}_${ARCH}
-      PROPERTIES EXCLUDE_FROM_ALL ON
-      )
+      PROPERTIES EXCLUDE_FROM_ALL ON)
   endforeach()
 endfunction()
 
@@ -165,19 +149,16 @@ macro(cmsdk_include_target TARGET CONFIG_LIST)
   else()
     set(ARCH_LIST link)
   endif()
-
   foreach(CONFIG ${CONFIG_LIST})
     foreach(ARCH ${ARCH_LIST})
       include(${TARGET}_${CONFIG}_${ARCH})
     endforeach(ARCH)
   endforeach(CONFIG)
-
 endmacro()
 
 include(${CMAKE_CURRENT_LIST_DIR}/sdk/cmsdk-app.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/sdk/cmsdk-bsp.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/sdk/cmsdk-lib.cmake)
-
 include(${CMAKE_CURRENT_LIST_DIR}/sdk/cmsdk2.cmake)
 
 
